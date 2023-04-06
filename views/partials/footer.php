a4b7b17b31b9016f1a1b79525a44703c5d24f8e6
Christophe_M2i <cduthoit@live.fr>
2023-04-06 08:58:45 +0200

    ok

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,3 +1,9 @@
 <?php
     /* echo "Footer"; */
 ?>
+
+<footer id="sticky-footer" class="flex-shrink-0 py-4 text-dark" style="background-color: #e3f2fd;">
+    <div class="container text-start">
+      <small>Copyright &copy; 2023 Mon site web</small>
+    </div>
+  </footer>
--- views/partials/header.php
+++ views/partials/header.php
@@ -2,9 +2,37 @@
     /* echo "Header";*/
 ?>
 
-<ul>
-    <li><a href="/views/acceuil.php">Accueil</a></li>
-    <li><a href="/views/catalogue.php">Catalogue</a></li>
-    <li><a href="/views/contact.php">Contact</a></li>
-    <li><a href="/views/login.php">Inscription</a></li>
-</ul>
+<!-- Ma navbar Bootstrap 5-->
+<!-- ajout du style background-color: #e3f2fd;" dans la balise nav de départ -->
+
+<nav class="navbar navbar-expand-lg fixed-top" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="/icones/Logo_Airbus_2014.svg" alt="Mon logo" width="200">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/views/acceuil.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/catalogue.php">Catalogue</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/login.php">Inscription</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
