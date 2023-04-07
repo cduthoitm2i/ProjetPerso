e99f0e525af3da3a849d80ea150eaece66a4458a
Christophe_M2i <cduthoit@live.fr>
2023-04-07 08:43:50 +0200

    ok

--- views/modele.php
+++ views/modele.php
@@ -11,7 +11,7 @@ http://localhost:3000/ProjetPersoInfoAvions/views/modele.php -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <!-- Ma CSS -->
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Mon modèle de site</title>
 </head>
 
@@ -27,8 +27,13 @@ http://localhost:3000/ProjetPersoInfoAvions/views/modele.php -->
         ?>
     </nav>
     <section>
-        <h1>Mon image</h1>
-        <img src="../images/europe.jpg" alt="Photo" />
+        <div class="container">
+            <h1>Mon image</h1>
+            <img src="../images/europe.jpg" alt="Photo" />
+            <br><br><br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br><br> <br><br><br><br><br><br><br><br><br><br>
+
+        </div>
+
     </section>
     <footer>
         <?php
--- views/partials/header.php
+++ views/partials/header.php
@@ -7,7 +7,7 @@
 
 <nav class="navbar navbar-expand-lg fixed-top" style="background-color: #e3f2fd;">
   <div class="container-fluid">
-  <a class="navbar-brand" href="/views/modele.php">
+  <a class="navbar-brand" href="../views/modele.php">
       <img src="../icones/Logo_Airbus_2014.svg" alt="Mon logo" width="200">
     </a>
 
@@ -17,19 +17,19 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/views/acceuil.php">Accueil</a>
+          <a class="nav-link active" aria-current="page" href="../views/accueil.php">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/views/catalogue.php">Catalogue</a>
+          <a class="nav-link" href="../views/catalogue.php">Catalogue</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/views/contact.php">Contact</a>
+          <a class="nav-link" href="../views/contact.php">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/views/inscription.php">Inscription</a>
+          <a class="nav-link" href="../views/inscription.php">Inscription</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/views/login.php">Login</a>
+          <a class="nav-link" href="../views/login.php">Login</a>
         </li>
       </ul>
       <form class="d-flex" role="search">
